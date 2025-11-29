60db8469c7f461f35a3eb61326df58d0f27b39e3
Joel-PYG <joel@risetopurpose.org>
2025-11-29 17:00:38 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/admin', // folder to copy
-          dest: ''          // copied to root of /dist
-        }
-      ]
-    })
-  ]
+  plugins: [react()]
 });
-
